7a9ea929c17db64a82500fa2f4852674035b72fd
Manir Uddin <manir22sust@gmail.com>
2024-01-03 16:24:19 +0100

    Loading, Error, Not Found Layouts

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import Links from "./links/Links";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div> logo</div>
+      <div>
+        <Links />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
