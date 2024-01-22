487e824e0891a06346d03ef48206b8ba05589ba3
Manir Uddin <manir22sust@gmail.com>
2024-01-22 23:24:44 +0100

    add images and configure with domain url

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,18 @@
+import Image from "next/image";
+import styles from "./about.module.css";
+
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div>
+      <div className={styles.imgContainer}>
+        <Image
+          src="https://images.unsplash.com/photo-1519827118356-a59fe24cd3ff?q=80&w=1176&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""
+          fill
+        />
+      </div>
+    </div>
+  );
 };
 
 export default AboutPage;
