841bce22938f51dec37f9ea2726bab32d56e18d0
Manir Uddin <manir22sust@gmail.com>
2024-01-22 20:55:41 +0100

    add temporary admin and login button

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -20,11 +20,22 @@ const Links = () => {
       path: "/blog",
     },
   ];
+  //  TEMPORARY
+  const session = true;
+  const isAdmin = true;
   return (
     <div className={styles.links}>
       {links.map((link) => (
         <NavLink item={link} key={link.title} />
       ))}
+      {session ? (
+        <>
+          {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
+          <button className={styles.logout}> Logout</button>
+        </>
+      ) : (
+        <NavLink item={{ title: "Login", path: "login" }} />
+      )}
     </div>
   );
 };
