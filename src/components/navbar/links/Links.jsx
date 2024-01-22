5b2c7a1382fcf0b9d6895132185d3ab67b11153d
Manir Uddin <manir22sust@gmail.com>
2024-01-22 22:29:47 +0100

    Implement responsive design for Navbar and Container
    - Updated CSS styles to create a responsive navbar that adjusts to different screen sizes.
    - Implemented media queries to enhance the layout's responsiveness on various devices.
    - Introduced a responsive container layout to ensure optimal display across different resolutions.

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,7 +1,10 @@
+"use client";
 import NavLink from "./navLink/NavLink";
 import styles from "./links.module.css";
+import { useState } from "react";
 
 const Links = () => {
+  const [open, setOpen] = useState();
   const links = [
     {
       title: "Home",
@@ -24,17 +27,32 @@ const Links = () => {
   const session = true;
   const isAdmin = true;
   return (
-    <div className={styles.links}>
-      {links.map((link) => (
-        <NavLink item={link} key={link.title} />
-      ))}
-      {session ? (
-        <>
-          {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
-          <button className={styles.logout}> Logout</button>
-        </>
-      ) : (
-        <NavLink item={{ title: "Login", path: "login" }} />
+    <div className={styles.container}>
+      <div className={styles.links}>
+        {links.map((link) => (
+          <NavLink item={link} key={link.title} />
+        ))}
+        {session ? (
+          <>
+            {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
+            <button className={styles.logout}> Logout</button>
+          </>
+        ) : (
+          <NavLink item={{ title: "Login", path: "login" }} />
+        )}
+      </div>
+      <button
+        className={styles.menuButton}
+        onClick={() => setOpen((prev) => !prev)}
+      >
+        Menu
+      </button>
+      {open && (
+        <div className={styles.mobileLinks}>
+          {links.map((link) => (
+            <NavLink item={link} key={link.title} />
+          ))}
+        </div>
       )}
     </div>
   );
