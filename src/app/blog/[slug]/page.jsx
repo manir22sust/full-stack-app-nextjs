7c37b3ae7b6718417b8ff12ee1d18600bc32f7d5
Manir Uddin <manir22sust@gmail.com>
2024-01-23 18:20:06 +0100

    Enhance Single Blog Page Design
    Implemented a new layout and styling for the single blog page, focusing on improving readability and visual appeal. Added a responsive design to ensure optimal viewing on various devices. Included share buttons for social media engagement and optimized images for faster loading. Also, improved the overall user experience by refining the navigation and typography on the page.

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,5 +1,45 @@
+import Image from "next/image";
+import styles from "./singlePost.module.css";
+
 const SinglePostPage = () => {
-  return <div>SinglePostPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          className={styles.img}
+          src="https://images.unsplash.com/photo-1593376853899-fbb47a057fa0?q=80&w=1315&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""
+          fill
+        />
+      </div>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Title</h1>
+        <div className={styles.detail}>
+          <Image
+            className={styles.avatar}
+            src="https://images.unsplash.com/photo-1533228876829-65c94e7b5025?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+            width={50}
+            height={50}
+          />
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Author</span>
+            <span className={styles.detailValue}>Terry Jefferson</span>
+          </div>
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Publish</span>
+            <span className={styles.detailValue}> 2024</span>
+          </div>
+        </div>
+        <div className={styles.content}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut
+          cupiditate voluptatum fuga hic. Esse temporibus optio deleniti
+          dignissimos ullam nobis id minus tenetur laboriosam quo! Excepturi
+          quidem alias delectus id.
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SinglePostPage;
--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -2,6 +2,7 @@
 import NavLink from "./navLink/NavLink";
 import styles from "./links.module.css";
 import { useState } from "react";
+import Image from "next/image";
 
 const Links = () => {
   const [open, setOpen] = useState();
@@ -41,12 +42,14 @@ const Links = () => {
           <NavLink item={{ title: "Login", path: "login" }} />
         )}
       </div>
-      <button
+      <Image
         className={styles.menuButton}
+        src="/menu.png"
+        alt=""
+        width={30}
+        height={30}
         onClick={() => setOpen((prev) => !prev)}
-      >
-        Menu
-      </button>
+      />
       {open && (
         <div className={styles.mobileLinks}>
           {links.map((link) => (
