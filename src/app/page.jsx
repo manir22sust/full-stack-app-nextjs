dbc93b4f5f33fe6f14c692577afaf5f574695d32
Manir Uddin <manir22sust@gmail.com>
2024-01-23 10:21:06 +0100

    Implement responsive and visually appealing home page design
    - I added a new home page design that incorporates a clean and modern layout. The design is responsive, ensuring a seamless user experience across various devices. I also optimized the images for faster loading and made improvements to the navigation menu for better usability.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,29 @@
+import Image from "next/image";
+import styles from "./home.module.css";
 const Home = () => {
-  return <div> Home</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}> Creative Thoughts Agency.</h1>
+        <p className={styles.desc}>
+          {" "}
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum,
+          numquam modi sequi placeat facere doloremque error ea magnam quasi
+          maiores animi! Eveniet, esse! Debitis adipisci aut, dolores quia non
+          nihil?
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}> Learn More</button>
+          <button className={styles.button}> Contact</button>
+        </div>
+        <div className={styles.brands}>
+          <Image src="/brands.png" alt="" fill className={styles.brandImg} />
+        </div>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image src="/hero.gif" alt="" fill className={styles.heroImg} />
+      </div>
+    </div>
+  );
 };
 export default Home;
